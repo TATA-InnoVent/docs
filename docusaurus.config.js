d7bdf0f2f1bfe9157b08d451ad4f6f8a399ebe5d
Nishantth1 <97899868+Nishantth1@users.noreply.github.com>
2024-09-07 06:59:56 +0000

    Updated docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,14 +16,13 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs',
-
+  baseUrl: '/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'TATA-InnoVent', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
   deploymentBranch: 'gh-pages',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
